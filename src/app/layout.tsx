8b87ee460b5c6374f848b0a84b510b65c1ea1e6a
Laura Lopez <lauralop_11@hotmail>
2025-04-01 11:23:18 -0400

    faid deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@components/Header";
 import Navbar from "@components/Navbar";
 import "./globals.css";
 
-export const medata: Metadata ={
+export const metadata: Metadata ={
   title: "Cuentas App",
   description: "App by create next app",
 }
