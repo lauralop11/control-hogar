96ea20e302d06f6db47d7f8d782856bb8cbc31d0
Felipe <jfelipecr94@gmail.com>
2025-07-02 14:33:25 -0400

    add savings page and card of savings

--- src/app/[type]/page.tsx
+++ src/app/[type]/page.tsx
@@ -6,6 +6,7 @@ import { Data} from "@app-types/types";
 import Acordion from "@components/ui/Acordion";
 import TotalForCategory from "@components/TotalForCategory";
 import BtnReturn from "@components/ui/BtnReturn";
+import SavingsPage from "sections/savings/page";
 
 type Params = {
   type: string;
@@ -37,17 +38,30 @@ const names = {
 const color = colors[type] || "text-primary";
 const name = names[type] || "valor";
 
-return (
-  <>
-    <div className="absolute top-3 right-4">
-      <BtnReturn/>
-    </div>
-    <section className=" flex flex-col items-center">
+function renderSection(sectionName:string) {
+
+  if (sectionName === 'Ahorro') {
+    return (
+      <SavingsPage />
+    )
+  }
+
+  return (
+    <section className="flex flex-col items-center">
       <h2 className={`${color} font-extrabold text-2xl flex gap-4 justify-center items-center`}>
           {name.toUpperCase()} <TotalForCategory type={type} />
       </h2>
       <Acordion data={data} type={type}/>
     </section>
+  )
+}
+
+return (
+  <>
+    <div className="absolute top-3 right-4">
+      <BtnReturn/>
+    </div>
+    { renderSection(name) }
   </>
 
 );
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="mb-10 relative h-15 shadow-sm bg-header text-white tracking-widest">
+    <header className="relative h-15 shadow-sm bg-header text-white tracking-widest">
       <div className="absolute top-0 left-0 w-full h-full flex justify-start items-center">
         <Link href="/" >
           <h2 className="font-bold text-2xl w-full cursor-pointer pl-5">Finanzas Hogar</h2>
