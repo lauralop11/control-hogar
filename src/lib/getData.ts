d9dbf5c9c094cbdaef9f12eca3c4fb6b1ac30e23
Laura Lopez <lauralop_11@hotmail>
2025-07-02 10:26:08 -0400

    deploy test 5

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -8,11 +8,13 @@ export async function getDataFilter(type: string) {
   const data = await res.json();
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
+  console.log("data sin filtro", data)
   const dataFilter: Data[] = (data || []).filter((item) => {
   const itemDate = new Date(item.date);
   const itemYear = itemDate.getFullYear();
   const itemMonth = itemDate.getMonth();
-  return (itemYear === currentYear && itemMonth === currentMonth);
+  console.log(currentMonth, currentYear, itemMonth, itemYear)
+  return itemYear === currentYear && itemMonth === currentMonth;
 });
   console.log("📦 Datos filtrados:", dataFilter);
   return dataFilter;
